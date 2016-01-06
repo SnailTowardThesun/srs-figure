5478e914e8086e5fff43eee5c7f7e92342fa7a33
SnailTowarThesun <hanvskun@hotmail.com>
2016-01-06 21:21:38 +0800

    rename the rtmp.h into srs_figure_rtmp_handshake.h in order to follow the SRS's style

---
+++ trunk/protocol/srs_figure_rtmp_handshake.h
@@ -0,0 +1,17 @@
+#ifndef __HEADER_RTMP_PROTOCOL__
+#define __HEADER_RTMP_PROTOCOL__
+// this is for rtmp protocol
+#include <iostream>
+using namespace std;
+
+typedef struct _tagRtmpC0
+{
+
+}Rtmp_c0,*pRtmp_c0;
+
+typedef struct _tagRtmpC1
+{
+}Rtmp_c1,*pRtmp_c1;
+
+
+#endif
