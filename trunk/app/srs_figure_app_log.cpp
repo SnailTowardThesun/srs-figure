cb74901932b2bdbcb489090c48cfb487874ed400
SnailTowardThesun <hanvskun@hotmail.com>
2016-01-08 16:45:20 +0800

    complete the log function

---
+++ trunk/app/srs_figure_app_log.cpp
@@ -0,0 +1,105 @@
+#include "srs_figure_app_log.h"
+#include <sys/time.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+srs_figure_log* srs_figure_log::pInstance = nullptr;
+
+srs_figure_log::srs_figure_log() : mLogFile(DEFAULT_SRS_FIGURE_LOG_FILE),
+		log_data(nullptr),
+		flogFile(-1)
+{
+	log_data = new char[LOG_MAX_SIZE];
+}
+
+srs_figure_log::~srs_figure_log()
+{
+	if(log_data != nullptr) delete log_data;
+}
+
+srs_figure_log* srs_figure_log::getInstance()
+{
+	if(pInstance == nullptr)
+			pInstance = new srs_figure_log();
+	return pInstance;
+}
+
+void srs_figure_log::setLogFile(const std::string strLogFile)
+{
+	mLogFile = strLogFile;
+}
+
+bool srs_figure_log::generate_header(const char* tag, int context_id, const char* level_name, int* header_size)
+{
+	// clock time
+    timeval tv;
+    if (gettimeofday(&tv, NULL) == -1) 
+	{
+        return false;
+    }
+    
+    // to calendar time
+    struct tm* tm;
+    
+	if ((tm = localtime(&tv.tv_sec)) == NULL) 
+	{
+		return false;
+	}
+    
+    // write log header
+    int log_header_size = -1;
+     
+    if (tag) 
+	{
+        log_header_size = snprintf(log_data, LOG_MAX_SIZE, 
+                "[%d-%02d-%02d %02d:%02d:%02d.%03d][%s][%s][%d][%d] ", 
+                1900 + tm->tm_year, 1 + tm->tm_mon, tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec, (int)(tv.tv_usec / 1000), 
+                level_name, tag, getpid(), context_id);
+    } 
+	else 
+	{
+        log_header_size = snprintf(log_data, LOG_MAX_SIZE, 
+                "[%d-%02d-%02d %02d:%02d:%02d.%03d][%s][%d][%d] ", 
+                1900 + tm->tm_year, 1 + tm->tm_mon, tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec, (int)(tv.tv_usec / 1000), 
+                level_name, getpid(), context_id);
+    }
+
+    if (log_header_size == -1) 
+	{
+        return false;
+    }
+    
+    // write the header size.
+    *header_size = LOG_MAX_SIZE - 1 < log_header_size ? LOG_MAX_SIZE-1 : log_header_size;
+    
+    return true;
+
+}
+
+long srs_figure_log::log(const char* level_name, const char* tag ,const char *fmt, ...)
+{
+	int header_size = 0,size = 0;
+	if(!generate_header(tag,0,level_name,&header_size))
+	{
+		return RESULT_ERROR;
+	}
+
+	va_list ap;
+	va_start(ap,fmt);
+	
+	size = header_size + vsnprintf(log_data + header_size ,LOG_MAX_SIZE - header_size,fmt,ap);
+	va_end(ap);
+
+	size = size > LOG_MAX_SIZE - 1 - 1 ? LOG_MAX_SIZE - 1 - 1 : size;
+
+    flogFile = ::open(mLogFile.c_str(), O_RDWR | O_APPEND);
+    
+    if(flogFile == -1) {
+        flogFile = ::open(mLogFile.c_str(), 
+            O_RDWR | O_CREAT | O_TRUNC, 
+            S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH
+        );
+    }
+
+	return RESULT_OK;
+}
---
+++ trunk/app/srs_figure_app_log.h
@@ -0,0 +1,34 @@
+// this srs-figure log part
+//
+#ifndef __HEADER_APP_LOG__
+#define __HEADER_APP_LOG__
+
+#include "../core/srs_figure_core.h"
+#include <iostream>
+using namespace std;
+
+#define DEFAULT_SRS_FIGURE_LOG_FILE "srs-figure.log"
+#define LOG_MAX_SIZE 4096
+
+
+class srs_figure_log
+{
+public:
+	srs_figure_log();
+	virtual ~srs_figure_log();
+public:
+	static srs_figure_log *pInstance;
+	static srs_figure_log *getInstance();
+private:
+	std::string mLogFile;
+	char* log_data;
+	int flogFile;
+protected:
+	bool generate_header(const char* tag, int context_id, const char* level_name, int* header_size);
+
+public:
+	void setLogFile(const std::string strLogFile);
+	long log(const char* level_name, const char* tag, const char *fmt,...);
+};
+
+#endif
