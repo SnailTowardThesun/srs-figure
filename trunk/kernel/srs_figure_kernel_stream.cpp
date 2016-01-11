9acfd9564df8b7d20ed86eb3177b0ac25cca31ca
SnailTowarThesun <hanvskun@hotmail.com>
2016-01-11 23:34:21 +0800

    add kernel stream

---
+++ trunk/kernel/srs_figure_kernel_stream.cpp
@@ -0,0 +1,11 @@
+#include "srs_figure_kernel_stream.h"
+
+srs_stream::srs_stream()
+{
+
+}
+
+srs_stream::~srs_stream()
+{
+
+}
---
+++ trunk/kernel/srs_figure_kernel_stream.h
@@ -0,0 +1,17 @@
+// this srs figure kernel part
+#ifndef __HEADER_KERNEL_STREAM__
+#define __HEADER_KERNEL_STREAM__
+
+#include "../core/srs_figure_core.h"
+#include "../app/srs_figure_app_socket.h"
+#include "../protocol/srs_figure_rtmp_handshake.h"
+using namespace std;
+class srs_stream
+{
+public:
+	srs_stream();
+	virtual ~srs_stream();
+
+};
+
+#endif
