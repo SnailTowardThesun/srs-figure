802abd9566eda8aa66ad35301ad7eee679ff7489
SnailTowardThesun <hanvskun@hotmail.com>
2016-01-14 17:50:22 +0800

    backup

--- trunk/protocol/srs_figure_rtmp_chunk.cpp
+++ trunk/protocol/srs_figure_rtmp_chunk.cpp
@@ -1,20 +1,103 @@
 #include "srs_figure_rtmp_chunk.h"
+#include "../app/srs_figure_app_log.h"
+
 using namespace srs_rtmp_chunk;
-rtmp_chunk::rtmp_chunk(unsigned long lChunkStreamID,unsigned char cMessageType):
+rtmp_chunk::rtmp_chunk(unsigned long lChunkStreamID,unsigned char cMessageType,long lTimeStamp):
 		mMessageStreamID(0),
 		mMessageStreamType(cMessageType),
-		mChunkStreamID(lChunkStreamID)
+		mChunkStreamID(lChunkStreamID),
+		mTimeStamp(lTimeStamp),
+		max_chunk_data_size(128),
+		mpBasicHeader(nullptr),
+		mChunkBasicHeader(basic_header_error)
 {
-	
+	// format mpBasicHeader
+	if(mChunkStreamID < 64)
+	{
+		mpBasicHeader = new char[1];
+		mpBasicHeader[0] = mChunkStreamID;
+	}
+	else if(mChunkStreamID < 319)
+	{
+		mpBasicHeader = new char[2];
+		mChunkBasicHeader = basic_header_type1;
+		
+		mpBasicHeader[0] = 0;
+		mpBasicHeader[1] = mChunkStreamID - 64;
+	}
+	else if(mChunkStreamID <= 65599)
+	{
+		mpBasicHeader = new char[3];
+		mChunkBasicHeader = basic_header_type2;
+
+		mpBasicHeader[0] = 1;
+		if(mChunkStreamID % 256 < 64)
+		{
+			mpBasicHeader[2] = mChunkStreamID / 256 - 1;
+			mpBasicHeader[1] = mChunkStreamID - mpBasicHeader[2] * 256 - 64;
+		}
+		else
+		{
+			mpBasicHeader[2] = mChunkStreamID / 256;
+			mpBasicHeader[1] = mChunkStreamID - mpBasicHeader[2] * 256 - 64;
+		}
+	}
+	else
+	{
+		mChunkBasicHeader = basic_header_error;
+		srs_figure_log::getInstance()->log("Error","rtmp chunk create","fail to initalize chunk header,the chunk stream ID %d is out of range",mChunkStreamID);
+	}
 }
 
 rtmp_chunk::~rtmp_chunk()
 {
-
+	if(mpBasicHeader != nullptr) delete[] mpBasicHeader;
 }
 
-std::vector<std::string> rtmp_chunk::AssembleOneChunk()
+std::vector<std::string> rtmp_chunk::AssembleOneControlChunk(std::string pMsg)
+{
+	mControlChunkList.clear();
+	
+
+	return mControlChunkList;
+}
+
+std::vector<std::string> rtmp_chunk::AssembleOneDataChunk(std::string pMsg)
 {
 	mChunkList.clear();
+	if(mChunkBasicHeader ==basic_header_error) return mChunkList;
+
 	return mChunkList;
+}
+
+long rtmp_chunk::AssembleHeader(std::string& msg)
+{
+	char* pMessageHeader = nullptr;
+	switch(mChunkState)
+	{
+	case UnInitialized:
+		mChunkState = FirstChunk;
+		// FMT is 0
+		mpBasicHeader[0] = mpBasicHeader[0] & 0x3F;
+		pMessageHeader = new char[11];
+		break;
+
+	case FirstChunk:
+		mChunkState = SecondChunk;
+		// FMT is 2
+		mpBasicHeader[0] = (mpBasicHeader[0] & 0x3F) | 0x80;
+		pMessageHeader = new char[3];
+		break;
+
+	case SecondChunk:
+		mChunkState = OtherChunk;
+		// FMT is 3
+		mpBasicHeader[0] = (mpBasicHeader[0] & 0x3F) | 0xC0;
+		break;
+
+	default:
+		break;
+	}
+	if(pMessageHeader != nullptr) delete[] pMessageHeader;
+	return RESULT_OK;
 }
--- trunk/protocol/srs_figure_rtmp_chunk.h
+++ trunk/protocol/srs_figure_rtmp_chunk.h
@@ -8,19 +8,43 @@ using namespace std;
 
 namespace srs_rtmp_chunk
 {
+	enum chunk_state
+	{
+		UnInitialized = 0,
+		FirstChunk = 1,
+		SecondChunk=2,
+		OtherChunk=3
+	};
+	enum basic_header_type
+	{
+		basic_header_type0 = 0,//2-63
+		basic_header_type1,// 64-319
+		basic_header_type2,// 64-65599
+		basic_header_error,
+	};
 	class rtmp_chunk
 	{
 	public:
-		rtmp_chunk(unsigned long lChunkStreamID,unsigned char cMessageType);
+		rtmp_chunk(unsigned long lChunkStreamID,unsigned char cMessageType,long lTimeStamp);
 		virtual ~rtmp_chunk();
 	private:
+		chunk_state mChunkState;
+		basic_header_type mChunkBasicHeader;
+		char* mpBasicHeader;
+		unsigned long mTimeStamp;
+
 		unsigned long mMessageStreamID;
 		unsigned char mMessageStreamType;
 		unsigned long mChunkStreamID;
-
+		int max_chunk_data_size;
+		
 		std::vector<std::string> mChunkList;
+		std::vector<std::string> mControlChunkList;
+	protectd:
+		long AssembleHeader(std::string& msg);
 	public:
-		std::vector<std::string> AssembleOneChunk();
+		std::vector<std::string> AssembleOneDataChunk(std::string pMsg);
+		std::vector<std::string> AssembleOneControlChunk(std::string pMsg);
 	};
 };
 
